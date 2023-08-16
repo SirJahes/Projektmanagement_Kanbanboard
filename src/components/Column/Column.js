339bddafb83a0c848641b0170d30b2b5b7dfeeb1
Jascha Hessenkamp <Jascha@hessenkamp.de>
2023-08-16 16:40:11 +0200

    Optisches Drag & Drop hinzugefügt

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,25 +1,48 @@
 import Card from "../Card/Card";
 import "./Column.scss";
 import { mapOrder } from "../../utilities/sorts";
+import { Container, Draggable } from "react-smooth-dnd";
 
 const Column = (props) => {
   const { column } = props;
   const cards = mapOrder(column.cards, column.cardOrder, "id");
+
+  const onCardDrop = (dropResult) => {
+    console.log(">>> inside onCardDrop: ", dropResult);
+  }
   return (
     <>
       <div className="column">
-        <header>{column.title}</header>
-        <ul className="card-list">
-          {cards &&
-            cards.length > 0 &&
-            cards.map((card, index) => {
-              return <Card key={card.id} card={card} />;
-            })}
-        </ul>
+        <header className="column-drag-handle">{column.title}</header>
+        <div className="card-list">
+          <Container
+            groupName="col"
+            onDrop={onCardDrop}
+            getChildPayload={(index) => cards[index]}
+            dragClass="card-ghost"
+            dropClass="card-ghost-drop"
+            dropPlaceholder={{
+              animationDuration: 150,
+              showOnTop: true,
+              className: "card-drop-preview",
+            }}
+            dropPlaceholderAnimationDuration={200}
+          >
+            {cards &&
+              cards.length > 0 &&
+              cards.map((card, index) => {
+                return (
+                  <Draggable key={card.id}>
+                    <Card card={card} />
+                  </Draggable>
+                )
+              })}
+          </Container>
+        </div>
         <footer>Add another card</footer>
       </div>
     </>
-  );
-};
+  )
+}
 
 export default Column;
