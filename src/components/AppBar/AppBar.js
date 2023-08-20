21a3ea52eca8b8f6f00dfd974e93b411b73a810c
Jascha Hessenkamp <Jascha@hessenkamp.de>
2023-08-20 12:20:03 +0200

    Suchfunktionsdummy wieder entfernt

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,14 +1,11 @@
 import "./AppBar.scss";
 import logo from "../../images/KanbaFlex.png";
-import SearchBar from "../searchbar/searchbar";
-
-
 
 const AppBar = () => {
+
   return (
     <nav className="navbar-app">
       <img className="logo" src={logo} alt="Logo" />
-      <SearchBar onSearch={(searchText) => console.log("Searching for:", searchText)} />
     </nav>
   );
 };
